865302d20f8b310c6c0a696a61953eded8318b4f
HarriesChen <info@mrchenhao.com>
2015-11-25 17:40:04 +0800

    add script to exprt dynamic framework

---
+++ Example/TBCrashKitFramework/TBCrashKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  TBCrashKitFramework.h
+//  TBCrashKitFramework
+//
+//  Created by ChenHao on 11/25/15.
+//  Copyright © 2015 HarriesChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBCrashKitFramework.
+FOUNDATION_EXPORT double TBCrashKitFrameworkVersionNumber;
+
+//! Project version string for TBCrashKitFramework.
+FOUNDATION_EXPORT const unsigned char TBCrashKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBCrashKitFramework/PublicHeader.h>
+
+
